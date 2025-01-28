4f39ecc1bd6845b5dac3a50baa692bf98ca0de75
MateuszWawrzyczek <162040044+MateuszWawrzyczek@users.noreply.github.com>
2025-01-28 16:20:04 +0100

    Update AddTimetableDto.cs

--- brygady/Models/Dtos/AddTimetableDto.cs
+++ brygady/Models/Dtos/AddTimetableDto.cs
@@ -5,11 +5,11 @@ namespace Brygady.Data
     public class AddTimetableDto
     {
         public int Id { get; set; }
-    public int LineId { get; set; }
-    //public string StopName { get; set; }
-    public int Direction { get; set; }
-    //public int Order { get; set; }
-    public List<TripTimeDto>? Trips { get; set; } = new List<TripTimeDto>();
+        public int LineId { get; set; }
+        //public string StopName { get; set; }
+        public int Direction { get; set; }
+        //public int Order { get; set; }
+        public List<TripTimeDto>? Trips { get; set; } = new List<TripTimeDto>();
     }
 
 
